e62174b788c81d42eba8b7176deb95f3395187a2
Nikita Efremov <heknt90@mail.ru>
2021-04-06 00:41:14 +0300

    настраиваем React

--- app/src/main.js
+++ app/src/main.js
@@ -1,1 +1,5 @@
-let voo = true;
+import React from "react";
+import ReactDOM from "react-dom";
+import Editor from "./components/editor";
+
+ReactDOM.render(<Editor />, document.getElementById("root"));
