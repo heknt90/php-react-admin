f52cb1c1db0c74088bf28518b8728e975fe5b331
Nikita Efremov <heknt90@mail.ru>
2021-05-11 19:35:51 +0300

    исправлена ситуация при загрузке изображения, если отсутствует директория

--- app/api/uploadImage.php
+++ app/api/uploadImage.php
@@ -4,6 +4,10 @@
             $fileExt = explode('/', $_FILES['image']["type"])[1];
             $fileName = uniqid() . '.' . $fileExt;
 
+            if (!is_dir('../../img/')) {
+                mkdir("../../img/");
+            }
+
             move_uploaded_file($_FILES["image"]["tmp_name"], "../../img/" . $fileName);
             echo json_encode(array("src" => $fileName));
         }
